7c76ac6f3b95d7766634df4eb003e31d79128f7a
Usman Kiani <usmankiani256@gmail.com>
2021-08-16 19:42:50 +0500

    update styles

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,10 +3,6 @@ import { Dimensions } from 'react-native'
 const { width, height } = Dimensions.get('window')
 
 const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
   width: width - 36,
   height: 40,
 }
