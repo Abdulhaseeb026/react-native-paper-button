82cf25f8f4af15ef4c9fefdb642801a2779c8be6
Usman Kiani <usmankiani256@gmail.com>
2021-08-16 17:48:27 +0500

    render paperbutton

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,8 @@
 import * as React from 'react'
-import { Button, NativeModules, StyleSheet, Text, View } from 'react-native'
+import PaperButton from './components/Button'
 
-export const addOne = (input: number) => input + 1
-
-export const Counter = () => {
-  const [count, setCount] = React.useState(0)
-
-  return (
-    <View style={styles.container}>
-      <Text>You pressed {count} times</Text>
-      <Button onPress={() => setCount(addOne(count))} title='Press Me' />
-    </View>
-  )
+const Button = () => {
+  return <PaperButton />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 200,
-  },
-})
-
-export default NativeModules.RNPaperButtonModule
+export default Button
