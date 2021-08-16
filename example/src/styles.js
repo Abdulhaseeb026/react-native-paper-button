b69a0ad49a24b68b4c2baf9403ae754d1441b667
Usman Kiani <usmankiani256@gmail.com>
2021-08-16 17:51:08 +0500

    add style to button view

---
+++ example/src/styles.js
@@ -0,0 +1,12 @@
+import { Dimensions } from 'react-native'
+
+const { width, height } = Dimensions.get('window')
+
+const styles = {
+  container: {
+    flex: 1,
+    alignSelf: 'center',
+  },
+}
+
+export default styles
