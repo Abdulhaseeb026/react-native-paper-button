876ba9891c8f4b7102693a75cd007778d1905531
Usman Kiani <usmankiani256@gmail.com>
2021-08-16 20:58:12 +0500

    update _labelStyle

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -5,6 +5,11 @@ const { width, height } = Dimensions.get('window')
 const styles = {
   width: width - 36,
   height: 40,
+
+  _labelStyle: {
+    height: 25,
+    width: 80,
+  },
 }
 
 export default styles
