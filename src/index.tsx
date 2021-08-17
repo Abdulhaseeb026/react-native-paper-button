8dfe8f5f1b5e9f58e0d5b5d1e9e7b9528f5a2807
Usman Kiani <usmankiani256@gmail.com>
2021-08-17 13:45:51 +0500

    export PaperButton

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,3 @@
-import * as React from 'react'
 import PaperButton from './components/Button'
 
-const Button = () => {
-  return <PaperButton />
-}
-
-export default Button
+export default PaperButton
